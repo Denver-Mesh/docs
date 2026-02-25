f1ef361b6ebd8f5dc9290fdd0ed5bbdcab34219a
Nate Harris <nwithan8@users.noreply.github.com>
2026-02-24 22:20:24 -0700

    - Debugging API call

--- MeshCore/nodes/sync_data.py
+++ MeshCore/nodes/sync_data.py
@@ -217,12 +217,17 @@ def _get_letsmesh_nodes() -> list[LetsMeshNode]:
                                  extract_list=True,
                                  sub_keys=["nodes"],
                                  headers={
-                                     "Host": "api.letsmesh.net",
-                                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:147.0) Gecko/20100101 Firefox/147.0",
-                                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                                     "Accept-Language": "en-US,en;q=0.9",
-                                     "Accept-Encoding": "gzip, deflate, br, zstd",
-                                     "Connection": "keep-alive",
+                                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                                     'Accept-Language': 'en-US,en;q=0.9',
+                                     'DNT': '1',
+                                     'Alt-Used': 'api.letsmesh.net',
+                                     'Connection': 'keep-alive',
+                                     'Upgrade-Insecure-Requests': '1',
+                                     'Sec-Fetch-Dest': 'document',
+                                     'Sec-Fetch-Mode': 'navigate',
+                                     'Sec-Fetch-Site': 'none',
+                                     'Sec-Fetch-User': '?1',
+                                     'Priority': 'u=0, i',
                                  })
 
 
